f1422ea1766713c8a60b33e90e43419b632f0866
Tho <nguyenhoangtrungtho9130@gmail.com>
2019-02-21 11:25:12 +0700

    show action controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,7 @@ class ProductsController < ApplicationController
     def index
         @products = Product.includes(:category).published
     end
+    def show
+        @product = Product.find(params[:id]) 
+    end
 end
