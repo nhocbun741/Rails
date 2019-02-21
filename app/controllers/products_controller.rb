67c9c0c9007b59a4353b033fef5f13de3b0709cd
Tho <nguyenhoangtrungtho9130@gmail.com>
2019-02-21 17:26:10 +0700

    after refacter

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,41 +1,68 @@
 class ProductsController < ApplicationController
+    before_action :product_params, only: [:create, :update]
+    before_action :find_product, only: [:show, :edit, :update,:destroy]
+    ### action index
     def index
         @products = Product.includes(:category).published
     end
     def show
-        @product = Product.find(params[:id]) 
+    
+    ### action new
     end
     def new
         @product = Product.new
     end
+
+    ### action create
     def create
-        product_params = params.require(:product).permit(:title, :description, :price, :published, :category, :level, :country)
-        @product = Product.new(product_params)
-        if @product.save
-            flash[:notice] = 'You have successfully created the product'
-            redirect_to products_path
+        @product =Product.new(product_params)
+        unless @product.save
+            flash.now[:notice] = messageFail(:create)
+            return render :new
         end
-            flash.now[:notice] = 'There is an error in your form'
-            render :new
+        redirect_to products_path, notice: messageSuccess(:create)
     end
+
+    ### action edit
     def edit      
-       @product = Product.find(params[:id])
         render :new
     end
+
+    ### action update
     def update
-        product_params = params.require(:product).permit(:title, :description, :price, :published, :category,:level, :country)
-        @product = Product.find(params[:id])
-        if @product.update(product_params)
-            flash[:notice] = 'You have successfully updated the product'
-            redirect_to products_path
-         end
-            flash.now[:notice] = 'There is an error in your form'
-            render :new
+        unless @product.update(product_params)
+            flash.now[:notice] = messageFail(:create)
+            return render :new
+        end
+        redirect_to products_path, notice: messageSuccess(:update)
     end
+
+    ### action destroy
     def destroy
-        Product.destroy(params[:id])
-        redirect_to products_path
-        
+        message = messageFail(:destroy)
+        message = messageSuccess(:destroy) if @product.destroy
+        redirect_to products_path, notice: message
     end
-    
+
+    private
+
+    ### product_params
+    def product_params
+        params.require(:product).permit(:title, :description, :price, :published, :category,:level, :country)
+    end
+
+    ### find_product
+    def find_product
+        @product = Product.find(params[:id])
+    end
+
+    ### message success
+    def messageSuccess(action)
+        "You have successfully #{action} the product"
+    end
+
+    ### message fail
+    def messageFail(action)
+        "There is an error when #{action}"    
+    end  
 end
