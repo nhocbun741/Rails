13c215767bd64dbf90956d53b5ca0496a426936b
Tho <nguyenhoangtrungtho9130@gmail.com>
2019-02-21 16:41:58 +0700

    edit delete refacter

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,8 +15,27 @@ class ProductsController < ApplicationController
             flash[:notice] = 'You have successfully created the product'
             redirect_to products_path
         end
-            flash[:notice] = 'There is an error in your form'
+            flash.now[:notice] = 'There is an error in your form'
             render :new
+    end
+    def edit      
+       @product = Product.find(params[:id])
+        render :new
+    end
+    def update
+        product_params = params.require(:product).permit(:title, :description, :price, :published, :category,:level, :country)
+        @product = Product.find(params[:id])
+        if @product.update(product_params)
+            flash[:notice] = 'You have successfully updated the product'
+            redirect_to products_path
+         end
+            flash.now[:notice] = 'There is an error in your form'
+            render :new
+    end
+    def destroy
+        Product.destroy(params[:id])
+        redirect_to products_path
+        
     end
     
 end
