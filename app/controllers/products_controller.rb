cb5c938ed5f3ac2be27b6ec82b89b40305458d6a
Tho <nguyenhoangtrungtho9130@gmail.com>
2019-02-21 15:49:03 +0700

    show errors message

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,8 +12,10 @@ class ProductsController < ApplicationController
         product_params = params.require(:product).permit(:title, :description, :price, :published, :category, :level, :country)
         @product = Product.new(product_params)
         if @product.save
+            flash[:notice] = 'You have successfully created the product'
             redirect_to products_path
         end
+            flash[:notice] = 'There is an error in your form'
             render :new
     end
     
